e766cee5ae1dc09e1bc6603c68ae7f0ad18a1250
Euan Chua <crxrrllubxjclybrlrzsyyvpmxasvlmlwngyvzzr@eu4nza.me>
2025-05-25 22:35:17 +0800

    updated horizontal padding and other stuff

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 export function Footer() {
   return (
-    <footer className="w-full bg-[#0072bc] text-white/80 text-sm">
-      <div className="px-24 pt-16 pb-8">
+    <div className="w-full bg-[#0072bc] text-white/80 text-sm">
+      <div className="px-96 pt-16 pb-8">
         <div className="flex flex-col md:flex-row md:justify-between md:items-start gap-12">
           {/* Logo + Description */}
           <div className="flex flex-col gap-6 max-w-lg">
-            <Link href="/" className="flex flex-row gap-4">
+            <Link href="/" className="flex flex-row items-center gap-4">
               <Image
-                src="/sti_logo.png"
+                src="/sti_logo.svg"
                 alt="Footer Logo"
                 priority
                 width={90}
@@ -31,6 +31,6 @@ export function Footer() {
           &copy; {new Date().getFullYear()} STI College Tagaytay
         </div>
       </div>
-    </footer>
+    </div>
   );
 }
